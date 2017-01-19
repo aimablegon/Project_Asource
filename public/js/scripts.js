5b110884bdbe4e9507c6feb82de939b9591ca056
aimablegon <aimablegon@gmail.com>
2017-01-19 19:27:00 +0900

    2017.01-19-19:26

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,55 @@
+function resizeFn(){
+
+  console.log($('#standardWidth').width());
+  $('.line_top').css({
+    left : '50%',
+    marginLeft : function(){
+        var courseBtn = $(this).css('width');
+        return -parseInt(courseBtn)/2 + 'px';
+    },
+    top :  function(){
+        var courseBtn = $(this).css('height');
+        return -parseInt(courseBtn)/2 + 'px';
+    },
+  });
+  $('.line_bottom').css({
+    left : '50%',
+    marginLeft : function(){
+        var courseBtn = $(this).css('width');
+        return -parseInt(courseBtn)/2 + 'px';
+    },
+    top :  function(){
+        var courseBtn = $(this).prev('.course_list').css('height');
+        return parseInt(courseBtn) -15 + 'px';
+    }
+  });
+
+
+  $('.box_left').css({
+      width : function(){
+          var lineWidth = $('#standardWidth').css('width');
+          return parseInt(lineWidth)/2 + 1 + 'px';
+      }
+  });
+  $('.box_right').css({
+      width : function(){
+          var lineWidth = $('#standardWidth').css('width');
+          return parseInt(lineWidth)/2 + 'px';
+      },
+      marginLeft : function(){
+          var lineWidth = $('#standardWidth').css('width');
+          return parseInt(lineWidth)/2 + 'px';
+      }
+  });
+  $('.circle').css({
+      left :"50%",
+      marginLeft : function(){
+          var courseBtn = $(this).css('width');
+          return -parseInt(courseBtn)/2 + 'px';
+      }
+  })
+}
+resizeFn();
 //Gnb
 $(function(){
     $('.gnb').click(function(){
@@ -14,61 +66,17 @@ $(function(){
 })
 // course_list
 /*resize*/
+
 $(window).resize(function(){
-    $('.line_top').css({
-      left : '50%',
-      marginLeft : function(){
-          var courseBtn = $(this).css('width');
-          return -parseInt(courseBtn)/2 + 'px';
-      },
-      top :  function(){
-          var courseBtn = $(this).css('height');
-          return -parseInt(courseBtn)/2 + 'px';
-      },
-    });
-    $('.line_bottom').css({
-      left : '50%',
-      marginLeft : function(){
-          var courseBtn = $(this).css('width');
-          return -parseInt(courseBtn)/2 + 'px';
-      },
-      top :  function(){
-          var courseBtn = $(this).prev('.course_list').css('height');
-          return parseInt(courseBtn) -15 + 'px';
-      }
-    });
-
-
-    $('.box_left').css({
-        width : function(){
-            var lineWidth = $('.course').css('width');
-            return parseInt(lineWidth)/2 + 1 + 'px';
-        }
-    });
-    $('.box_right').css({
-        width : function(){
-            var lineWidth = $('.course').css('width');
-            return parseInt(lineWidth)/2 + 'px';
-        },
-        marginLeft : function(){
-            var lineWidth = $('.course').css('width');
-            return parseInt(lineWidth)/2 + 'px';
-        }
-    });
-    $('.circle').css({
-        left :"50%",
-        marginLeft : function(){
-            var courseBtn = $(this).css('width');
-            return -parseInt(courseBtn)/2 + 'px';
-        }
-    })
-})
+  resizeFn();
+});
 /*click*/
 $(function(){
     $('.btn_view').click(function(){
         $('.container_bottom').css({
             display : 'block'
         })
+        resizeFn();
     })
 
     $('.btn_new').click(function(){
@@ -77,6 +85,7 @@ $(function(){
         }).next('.new').css({
             display : 'block'
         })
+        resizeFn();
     });
     $('.btn_old').click(function(){
         $(this).parents('.new').css({
@@ -84,5 +93,6 @@ $(function(){
         }).prev('.old').css({
             display : 'block'
         })
+        resizeFn();
     })
 })
